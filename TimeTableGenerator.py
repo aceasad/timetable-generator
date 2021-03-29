e2370068bf9a545a7f9159ee5e4fdb559ad1a3f3
Asad Saeed <asad.saeed@live.com>
2021-03-30 00:23:43 +0500

    line rror fixed

--- TimeTableGenerator.py
+++ TimeTableGenerator.py
@@ -255,7 +255,7 @@ def get_courses():
         with open('student_courses.html', 'w') as file:
             file.write(html_courses)
         return render_pdf(HTML(string=html_courses))
-   except:
+    except:
         return "<div> <h1>Course Recommender</h1><br/><h2> Please request after waiting for sometime.</h2></div>"
 
 
